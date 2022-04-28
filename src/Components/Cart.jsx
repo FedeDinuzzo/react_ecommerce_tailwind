bf31238e77e4e8eea113e0c7fe041b108da653ab
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2022-04-27 23:58:58 -0300

    fix

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -10,11 +10,11 @@ const Cart = (product) => {
     <div className="fondo -m-16 h-16 mb-16"></div>
     <h3 className="">Productos en carrito: {cart.length}</h3>
         <div key={product.id} className="grid grid-cols-2 justify-center text-center m-8">
-            <img src={cart.image} alt="producto" className="m-auto" />
+            <img src={product.image} alt="producto" className="m-auto" />
             <div>
-                <h3 className="">${cart.price}</h3>
-                <h3 className="">{cart.name}</h3>
-                <h3 className="">{cart.count}</h3>
+                <h3 className="">${product.price}</h3>
+                <h3 className="">{product.name}</h3>
+                <h3 className="">{product.count}</h3>
                 <button onClick={() => removeItem(product.id)}>Remove Product</button>
             </div>
         </div>
