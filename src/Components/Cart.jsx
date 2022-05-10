b8000c94fd32eda1198590ea005e1ec96124e460
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2022-05-10 14:16:11 -0300

    cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react'
 import { Context } from '../CartContext/ContextProvider';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    let { cart, removeItem, clear, buyAll } = useContext(Context);
+  let { cart, removeItem, clear, buyAll } = useContext(Context);
 
   return (
     <>
     <div className="fondo -m-16 h-16 mb-16"></div>
-    <h3 className="">Productos en carrito: {cart.length}</h3>
     {cart.map((product, index) => (
         <div key={index} className="grid grid-cols-2 justify-center text-center m-8">
             <img src={product.image} alt="producto" className="m-auto" />
@@ -19,8 +19,9 @@ const Cart = () => {
             </div>
         </div>
     ))}
-    <button onClick={buyAll}>buy all</button>
-    <button onClick={() => clear(cart.id)}>Remove all</button>
+    <p><button onClick={buyAll}>buy all</button></p>
+    <p><button onClick={() => clear(cart.id)}>Remove all</button></p>
+    { cart.length === 0 ? <Link to={`/`}><button onClick={buyAll}>Back to Shop</button></Link> : <button onClick={buyAll}>Finish Checkout</button> }
     </>
   )
 }
--- src/Components/Navbar/CartWidget.jsx
+++ src/Components/Navbar/CartWidget.jsx
@@ -1,22 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { Context } from '../../CartContext/ContextProvider';
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const COMMON_STYLES = " flex justify-center items-center rounded-full text-white"; 
 
 export default function CartWidget({ className }) {
 
+    let location = useLocation();
     let { cart } = useContext(Context);
+    
+    const allQuantity = cart.map((product) => Number(product.quantity)).reduce((a, b) => a + b, 0)
 
     return (
         <>
         <Link to={`/cart`}>
         <button className = {className + COMMON_STYLES + "w-6 h-7 bg-violet-600"}>
             <AiOutlineShoppingCart class="w-5 h-5 mx-1"/>
-            <div className = {COMMON_STYLES + "w-5 h-5 p-1 bg-violet-900 text-xs font-semibold"}>
-                {cart.length <= 9 ? cart.length : "9+"}
-            </div>
+            {cart.length === 0 ? "" 
+            : (<div className = {COMMON_STYLES + "w-5 h-5 p-1 bg-violet-900 text-xs font-semibold"}>
+                {location.pathname === "/cart" ? allQuantity : cart.length }
+            </div>)}
         </button>
         </Link>
         </>
