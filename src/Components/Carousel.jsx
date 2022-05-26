5176d3384857187c44fc3d59cf2636ba7245915a
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2022-05-25 23:42:35 -0300

    carousel fix

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -5,12 +5,15 @@ function Carousel({ product }) {
     const [img, setImg] = useState(product.img[0]);
     const [loaded, setLoaded] = useState(false);
 
+    //Unified logic to use in prev and next buttons
     const newImg = (index, next = true) => {
         setLoaded(false);
-        const condition = next ? index < product.img.length - 1 : index > 0;
-        const nextIndex = next ? (condition ? index + 1 : 0) : (condition ? index - 1 : product.img.length - 1);
-        setImg(product.img[nextIndex]);
-        setIndex(nextIndex);
+        setTimeout(() => {
+            const condition = next ? index < product.img.length - 1 : index > 0;
+            const nextIndex = next ? (condition ? index + 1 : 0) : (condition ? index - 1 : product.img.length - 1);
+            setImg(product.img[nextIndex]);
+            setIndex(nextIndex);
+        }, 200);       
     };
 
     const previous = () => {
@@ -19,6 +22,12 @@ function Carousel({ product }) {
 
     const next = () => {
         newImg(index);
+    };
+
+    //Change the image according to the small one and update the index to keep it when using newImg
+    const NewImgThumb = (index) => {
+        setImg(product.img[index]);
+        setIndex(index);
     };
 
   return (
@@ -32,7 +41,7 @@ function Carousel({ product }) {
         <div className="grid grid-cols-3 md:grid-cols-6 my-2 m-auto w-80 md:w-3/4 lg:w-full xl:max-w-2xl">
             {product.imgMin.map((img, index)=> ( 
             <img src={img} key={index} alt=""
-            onClick={() => setImg(product.img[index])}
+            onClick={() => NewImgThumb(index)}
             className="hover:bg-gray-200 mx-auto h-20 md:h-auto my-1 border-2 cursor-pointer rounded-md" />
             ))}
         </div>
